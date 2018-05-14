524000d6a0477c58a0f52d275a9c479d2e745a7c
Tom Maarleveld <tom_maarleveld@live.nl>
2018-05-14 15:20:37 +0200

    Player movement
    No animations

---
+++ Redkiwi Map/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	[SerializeField]
+	private float speed;
+
+	private Vector2 direction;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetInput ();
+		Move ();
+	}
+
+	public void Move(){
+		transform.Translate (direction*speed*Time.deltaTime);
+	}
+
+	private void GetInput(){
+
+		direction = Vector2.zero;
+
+		if (Input.GetKey (KeyCode.W)) {
+			direction += Vector2.up;
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			direction += Vector2.left;
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			direction += Vector2.down;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			direction += Vector2.right;
+		}
+	}
+}
