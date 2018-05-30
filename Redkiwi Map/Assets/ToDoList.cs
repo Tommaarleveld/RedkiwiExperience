739771afe3eac4b000c49afb6c617ebb9e38e0a6
Ruben de Man <rubenodeman@gmail.com>
2018-05-30 10:50:40 +0200

    Added dynamic toggles

--- Redkiwi Map/Assets/ToDoList.cs
+++ Redkiwi Map/Assets/ToDoList.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ToDoList : MonoBehaviour {
 
     public static bool toDoListIsOpen = false;
 
+    public static bool task1 = true;
+    public static bool task2 = false;
+    public static bool task3 = false;
+
     public GameObject toDoListUI;
+
+    public Toggle toDoListTask1;
+    public Toggle toDoListTask2;
+    public Toggle toDoListTask3;
 	
 	// Update is called once per frame
 	void Update () {
@@ -23,6 +32,16 @@ public class ToDoList : MonoBehaviour {
         toDoListUI.SetActive(true);
         Time.timeScale = 0f;
         toDoListIsOpen = true;
+
+        if(task1){
+            toDoListTask1.isOn = true;
+        }
+        if(task2){
+            toDoListTask2.isOn = true;
+        }
+        if(task3){
+            toDoListTask3.isOn = true;
+        }
     }
 
     public void Resume(){
